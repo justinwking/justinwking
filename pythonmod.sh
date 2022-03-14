4f5f6a46230b00d6e96fbf3bb2a0775e3d077bf2
Justin W King <justinwking@me.com>
2022-03-14 18:15:12 -0400

    Update pythonmod.sh

--- pythonmod.sh
+++ pythonmod.sh
@@ -38,8 +38,8 @@ install_python () {
 
     if [ "$old_version" > "$new_version" ]; then
         echo "You are trying to install an older version than your current version!"
-        echo $old_version
-        echo $new_version
+        printf 'the old version is %s.' "$old_version"
+        printf 'the new version is %s.' "$new_version"
         echo "Exiting this script!"
         exit 0
     elif [ "$old_version" = "$new_version" ]; then
